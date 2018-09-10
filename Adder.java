60addc0777194b637d4bd2ee34377c39f1bbe60b
Thanyaporn <60050183@kmitl.ac.th>
2018-09-10 18:59:07 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Adder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("X = ");
+        int x = sc.nextInt();
+        System.out.print("Y = ");
+        int y = sc.nextInt();
+        int z = x/y;
+        System.out.println("Sum = "+z);
+    }
+}
